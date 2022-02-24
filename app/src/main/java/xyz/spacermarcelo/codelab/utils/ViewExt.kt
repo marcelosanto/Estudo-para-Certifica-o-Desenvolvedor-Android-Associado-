545fad11f3a47525a4a74d2f4c4ab9f3e50f83fb
Marcelo Santos <rocha.mer21@gmail.com>
2022-02-24 20:06:49 -0300

    criado a utils de View para animação

---
+++ app/src/main/java/xyz/spacermarcelo/codelab/utils/ViewExt.kt
@@ -0,0 +1,22 @@
+package xyz.spacermarcelo.codelab.utils
+
+import android.view.View
+import android.view.animation.Animation
+
+fun View.startAnimation(anim: Animation, onEnd: () -> Unit) {
+    anim.setAnimationListener(object : Animation.AnimationListener {
+        override fun onAnimationStart(anim: Animation?)  = onEnd()
+
+        override fun onAnimationEnd(anim: Animation?)  = Unit
+
+
+        override fun onAnimationRepeat(anim: Animation?) = Unit
+
+    })
+
+    this.startAnimation(anim)
+}
+
+fun View.setVisible(show: Boolean){
+    if (show) this.visibility = View.VISIBLE else this.visibility = View.GONE
+}
