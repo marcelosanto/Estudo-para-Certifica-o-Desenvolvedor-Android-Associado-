3ca7ac21196ca85668f8e06c3bc12bcc970113ba
Marcelo Santos <rocha.mer21@gmail.com>
2022-02-24 21:46:36 -0300

    criado a primeira lição com Toast e Snackbar

--- app/src/main/java/xyz/spacermarcelo/codelab/MainFragment.kt
+++ app/src/main/java/xyz/spacermarcelo/codelab/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import xyz.spacermarcelo.codelab.databinding.FragmentMainBinding
+import xyz.spacermarcelo.codelab.utils.navTo
 
 
 class MainFragment : androidx.fragment.app.Fragment(R.layout.fragment_main) {
@@ -14,7 +15,10 @@ class MainFragment : androidx.fragment.app.Fragment(R.layout.fragment_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding = FragmentMainBinding.bind(view)
+        binding.codelabToastSnake.setOnClickListener { navTo(R.id.toastSnakeFragment) }
+
     }
 
 }
---
+++ app/src/main/java/xyz/spacermarcelo/codelab/core/toastSnakeFragment.kt
@@ -0,0 +1,47 @@
+package xyz.spacermarcelo.codelab.core
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import xyz.spacermarcelo.codelab.R
+import xyz.spacermarcelo.codelab.databinding.FragmentMainBinding
+import xyz.spacermarcelo.codelab.databinding.FragmentToastSnakeBinding
+import xyz.spacermarcelo.codelab.utils.toast
+
+class ToastSnakeFragment : androidx.fragment.app.Fragment(R.layout.fragment_toast_snake) {
+
+    private lateinit var binding: FragmentToastSnakeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
+        binding = FragmentToastSnakeBinding.bind(view)
+
+        binding.toast.setOnClickListener {
+            val msg = "Minha Mensagem para você!"
+            Toast
+                .makeText(requireContext(), msg, Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        binding.snake.setOnClickListener {
+            Snackbar.make(view ,"Oi Snake", Snackbar.LENGTH_SHORT).show()
+        }
+
+        binding.blank.setOnClickListener {
+            Snackbar
+                .make(view, "Snake with Action", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OKAY") { toast("I am a snake!") }
+                .show()
+        }
+
+    }
+
+}
